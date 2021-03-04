7392e4a59f76fa2497447c0083bcbcda20500c18
Fadi Elsayed <fadi.fgf@gmail.com>
2021-03-04 10:37:13 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my1travelappui/Screens/HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
